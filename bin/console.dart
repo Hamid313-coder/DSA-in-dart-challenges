71cb7b148160a26efdd8b8c70f57251ebbadf7dc
hamid313-coder <hamid.hussainy0@gmail.com>
2024-01-06 14:13:45 +0430

    initial commit

---
+++ bin/console.dart
@@ -0,0 +1,7 @@
+import 'package:console/chapter-04-challenges/balanced_paranthesis/balanced_paranthesis.dart';
+
+void main(List<String> arguments) {
+  final text = 'lks(sfjalfa;a)skjfasjda;())';
+
+  print(isBalancedParanthesis(text));
+}
